ecc1cf411efee1c40c3a164aa84ce4b35e6d37a6
dudwlss <130740710+dudwlss@users.noreply.github.com>
2023-05-27 12:17:30 +0900

    Create 08-01.c

---
+++ 23-05-27/08-01.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int even(int n) {
+	if (n % 2 == 0)
+		return 1;
+	else
+		return 0;
+}
+
+int absoulte(int n) {
+	if (n < 0) 
+		return n * -1;
+	else
+		return n;
+}
+
+int sign(int n) {
+	if (n < 0)
+		return -1;
+	else if (n > 0)
+		return 1;
+	else
+		return 0;
+
+}
+
+int main() {
+	int n;
+
+	printf("정수를 입력하세요 : ");
+	scanf("%d", &n);
+
+	printf("even()의 결과: %d\n", even(n));
+	printf("absolute()의 결과: %d\n", absoulte(n));
+	printf("sign()의 결과: %d\n", sign(n));
+
+	return 0;
+}
