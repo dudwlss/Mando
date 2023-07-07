161f8376507c6c8b8ca21247349aa00f6b0bb7a7
dudwlss <130740710+dudwlss@users.noreply.github.com>
2023-07-07 21:04:50 +0900

    Create 배열 병합 프로그램.c

---
+++ 2023.07.06/배열 병합 프로그램.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void sol_arrayA(int a[]);
+void sol_arrayB(int b[]);
+void abs_array(int a[], int b[], int c[], int size);
+
+int main(void) {
+    srand((unsigned)time(NULL));
+
+    int a[50];
+    int b[50];
+    int c[100];
+    int i, j, size;
+
+    printf("A = ");
+    for (i = 0; i < 10; i++) {
+        a[i] = rand() % 100;
+    }
+
+    sol_arrayA(a);
+
+    for (i = 0; i < 10; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    printf("B = ");
+    for (i = 0; i < 10; i++) {
+        b[i] = rand() % 100;
+    }
+
+    sol_arrayB(b);
+
+    for (i = 0; i < 10; i++) {
+        printf("%d ", b[i]);
+    }
+    printf("\n");
+
+    abs_array(a, b, c, 20);
+    printf("C = ");
+    for (i = 0; i < 20; i++) {
+        printf("%d ", c[i]);
+    }
+    return 0;
+}
+
+void sol_arrayA(int a[]) {
+    int i, j, temp, min;
+    for (i = 0; i < 10 - 1; i++) {
+        min = i;
+        for (j = i + 1; j < 10; j++) {
+            if (a[j] < a[min]) {
+                min = j;
+            }
+        }
+        temp = a[i];
+        a[i] = a[min];
+        a[min] = temp;
+    }
+}
+void sol_arrayB(int b[]) {
+    int i, j, temp, min;
+    for (i = 0; i < 10 - 1; i++) {
+        min = i;
+        for (j = i + 1; j < 10; j++) {
+            if (b[j] < b[min]) {
+                min = j;
+            }
+        }
+        temp = b[i];
+        b[i] = b[min];
+        b[min] = temp;
+    }
+}
+void abs_array(int a[], int b[], int c[], int size) {
+    int j = 0;
+    int k = 0;
+    for (int i = 0; i < size; i++) {
+        if (j < size / 2 && k < size / 2) {
+            if (a[j] < b[k]) {
+                c[i] = a[j++];
+            }
+            else {
+                c[i] = b[k++];
+            }
+        }
+        else if (j == size / 2) {
+            c[i] = b[k++];
+        }
+        else if (k == size / 2) {
+            c[i] = a[j++];
+        }
+    }
+}
