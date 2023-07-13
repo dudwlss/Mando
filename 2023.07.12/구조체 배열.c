b976351498b0964c17936208f58eb10f14d772d1
dudwlss <130740710+dudwlss@users.noreply.github.com>
2023-07-13 17:10:48 +0900

    Update 구조체 배열.c

--- 2023.07.12/구조체 배열.c
+++ 2023.07.12/구조체 배열.c
@@ -12,17 +12,17 @@ struct Student {
     int rank;      
 };
 
-int generateRandomScore(int max) {
+int RandomScore(int max) {
     return rand() % (max + 1);
 }
 
-void calculateTotalScore(struct Student* students, int numStudents) {
+void TotalScore(struct Student* students, int numStudents) {
     for (int i = 0; i < numStudents; i++) {
         students[i].total = students[i].midterm + students[i].final + students[i].assignment;
     }
 }
 
-void calculateRank(struct Student* students, int numStudents) {
+void Rank(struct Student* students, int numStudents) {
     for (int i = 0; i < numStudents; i++) {
         students[i].rank = 1;
         for (int j = 0; j < numStudents; j++) {
@@ -40,14 +40,14 @@ int main() {
     struct Student students[NUM_STUDENTS];
     
     for (int i = 0; i < NUM_STUDENTS; i++) {
-        students[i].midterm = generateRandomScore(40);
-        students[i].final = generateRandomScore(30);
-        students[i].assignment = generateRandomScore(30);
+        students[i].midterm = RandomScore(40);
+        students[i].final = RandomScore(30);
+        students[i].assignment = RandomScore(30);
     }
 
-    calculateTotalScore(students, NUM_STUDENTS);
+    TotalScore(students, NUM_STUDENTS);
     
-    calculateRank(students, NUM_STUDENTS);
+    Rank(students, NUM_STUDENTS);
     
     printf("성적 처리 결과:\n");
     printf("번호\t중간\t기말\t과제\t총점\t석차\n");
